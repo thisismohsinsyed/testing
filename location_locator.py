3dd010906942f8a6e2b8864b604348c162fec611
Syed Mohsin Ali Shah <96146988+thisismohsinsyed@users.noreply.github.com>
2024-09-13 08:12:50 +0200

    location_locator.py

---
+++ location_locator.py
@@ -0,0 +1,63 @@
+
+import requests
+# Set your Google Maps API key here
+API_KEY = "AIzaSyAbt3a_IHhehoMhFs4ZkIuCB75TeM4D1VA"
+
+
+
+
+def get_location(city_name):
+    """Retrieve the location data from Google Maps API based on city name."""
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={city_name}&key={API_KEY}"
+    response = requests.get(url).json()
+    if response['status'] == 'OK':
+        return response['results'][0]['geometry']['location']
+    else:
+        return None
+
+def create_map_html(lat, lng):
+    """Generates HTML to embed Google Maps with a marker and enables clicking on the map to get coordinates."""
+    return f"""
+    <html>
+    <head>
+        <style>
+            #map {{
+                height: 400px;
+                width: 100%;
+            }}
+        </style>
+        <script src="https://maps.googleapis.com/maps/api/js?key={API_KEY}"></script>
+        <script>
+            function initMap() {{
+                var location = {{lat: {lat}, lng: {lng}}};
+                var map = new google.maps.Map(document.getElementById('map'), {{
+                    zoom: 15,
+                    center: location
+                }});
+                var marker = new google.maps.Marker({{
+                    position: location,
+                    map: map
+                }});
+
+                // Add a click event listener to the map
+                map.addListener('click', function(e) {{
+                    placeMarkerAndPanTo(e.latLng, map);
+                }});
+            }}
+
+            // Function to place a marker and pan to the new location
+            function placeMarkerAndPanTo(latLng, map) {{
+                var marker = new google.maps.Marker({{
+                    position: latLng,
+                    map: map
+                }});
+                map.panTo(latLng);
+                alert('Lat: ' + latLng.lat() + '\\nLng: ' + latLng.lng());  // Display coordinates in an alert
+            }}
+        </script>
+    </head>
+    <body onload="initMap()">
+        <div id="map"></div>
+    </body>
+    </html>
+    """
